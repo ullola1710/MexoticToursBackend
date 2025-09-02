6b1a2b43fbdfd64455f1e604afd21a781256ee72
Brandy Medina <medinabrandy034@gmail.com>
2025-09-02 12:53:10 -0600

    Separación por carpetas y adición de initializr

---
+++ Spring Boot/mexoticTours/src/main/java/com/mexotic/mexoticTours/MexoticToursApplication.java
@@ -0,0 +1,13 @@
+package com.mexotic.mexoticTours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MexoticToursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MexoticToursApplication.class, args);
+	}
+
+}
