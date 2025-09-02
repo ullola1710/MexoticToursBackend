90ab6de976bb8fc6f52a71bae036ff725c05062b
Brandy Medina <medinabrandy034@gmail.com>
2025-09-02 14:21:26 -0600

    Nueva version de initializr

--- Spring Boot/mexotic/src/main/java/com/mexotic/mexotic/MexoticApplication.java
+++ Spring Boot/mexotic/src/main/java/com/mexotic/mexotic/MexoticApplication.java
@@ -1,13 +1,13 @@
-package com.mexotic.mexoticTours;
+package com.mexotic.mexotic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MexoticToursApplication {
+public class MexoticApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MexoticToursApplication.class, args);
+		SpringApplication.run(MexoticApplication.class, args);
 	}
 
 }
