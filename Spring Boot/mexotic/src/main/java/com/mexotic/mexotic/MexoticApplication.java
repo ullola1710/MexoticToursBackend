57db8bc4669009b12faf0ff97d4e9b9327ca89f8
Brandy_Medina <medinabrandy034@gmail.com>
2025-09-02 15:49:19 -0600

    Nuevo initializr (#2)
    * Separación por carpetas y adición de initializr
    
    * Nueva version de initializr
    
    * nuevo initializr

---
+++ Spring Boot/mexotic/src/main/java/com/mexotic/mexotic/MexoticApplication.java
@@ -0,0 +1,13 @@
+package com.mexotic.mexotic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MexoticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MexoticApplication.class, args);
+	}
+
+}
